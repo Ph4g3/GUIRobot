7f90de013ede13ab9c9036db7ef862e89133b6ae
Martin Moore <thepuddingman@gmail.com>
2011-02-26 16:31:53 +0000

    Quick commit - closes #1.
    Should have been closed a while ago. Also fixed an issue with default directory
    trying to initialize logging.

--- __init__.py
+++ __init__.py
@@ -7,11 +7,15 @@ import os
 import sys
 
 #Add my own directory to sys.path so Python can find it
-directory = 'C:/Users/Ph4g3/workspace/'
-if os.path.isdir(directory):
-    sys.path.append(directory)
+mydir = 'C:/Users/Ph4g3/workspace/'
+if os.path.isdir(mydir):
+    sys.path.append(mydir)
+    directory = mydir
+#Otherwise default location is in python27
+else:
+    directory = os.getcwd()
 
 #Start our logging from file configuration
-logging.config.fileConfig(directory+'GUIRobot/logger.conf')
+logging.config.fileConfig(directory+'/GUIRobot/logger.conf')
 log = logging.getLogger('GUIRobot')
 log.debug('Logging initialized.')
